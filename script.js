47282256ba7d89e2f4a82057f1bca3d51a343ab1
Zena-art <zena.nazim@gmail.com>
2024-09-16 22:39:50 -0500

    Part 1 is done

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+
+
+//Part 1
+
+
+
+
 const str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
 
 // Convert the string into array
