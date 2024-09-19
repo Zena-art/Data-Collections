d1c8b38e66379674b8bfc9abfd1cd680162fd43e
Zena-art <zena.nazim@gmail.com>
2024-09-18 19:45:53 -0500

    comments added

--- script.js
+++ script.js
@@ -33,9 +33,12 @@ for (let i = 1; i < lines.length; i++) {
 
 console.log(result);
 
-const csv_to_array = (data, delimiter = ',') =>
-    data.split('\n').
-    map(v => v.split(delimiter));
 
 
+
+const csv_to_array = (data, delimiter = ',') => // Array function with 2 arguments, passing ',' as a value to a delimiter
+    data.split('\n'). // converting string into an array, splitting at new line char
+    map(v => v.split(delimiter)); //using callback function of map to create nested arrays and passing v (value) splitting and using delimiter to separate the values
+
+    // invoking my function passing str from part 1 task.
     console.log(csv_to_array(str));
