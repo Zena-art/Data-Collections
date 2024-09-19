2347bf9e031dd6b9d06647f93064b4678f3f71fe
Zena-art <zena.nazim@gmail.com>
2024-09-18 19:35:07 -0500

    seond part code is working

--- script.js
+++ script.js
@@ -33,4 +33,9 @@ for (let i = 1; i < lines.length; i++) {
 
 console.log(result);
 
+const csv_to_array = (data, delimiter = ',') =>
+    data.split('\n').
+    map(v => v.split(delimiter));
 
+
+    console.log(csv_to_array(str));
